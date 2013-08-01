2ce597d124afdccb659e131f00abf25282a4feec
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2013-08-01 21:00:44 +0200

    improved viewer

--- myviewer.py
+++ myviewer.py
@@ -2,28 +2,145 @@ import numpy as np
 from pyqtgraph.Qt import QtGui, QtCore
 import pyqtgraph as pg
 from collections import OrderedDict
+import vigra
 
+
+
+sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding,
+                                         QtGui.QSizePolicy.Expanding)
+sizePolicy.setHorizontalStretch(0)
+sizePolicy.setVerticalStretch(0)
+
+
+def setExpanding(widget):
+    sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding,
+                                         QtGui.QSizePolicy.Expanding)
+    sizePolicy.setHorizontalStretch(0)
+    sizePolicy.setVerticalStretch(0)
+
+    widget.setSizePolicy(sizePolicy)
+
+def setPolicy(widget,p=QtGui.QSizePolicy.Maximum):
+    
+    sizePolicy = QtGui.QSizePolicy(p,p)
+    sizePolicy.setHorizontalStretch(0)
+    sizePolicy.setVerticalStretch(0)
+
+    widget.setSizePolicy(sizePolicy)
 
 class Layer(object):
 
     class Ui(object):
-        def __init__(self):
-            self.layout=None
+        def __init__(self,parent):
+            self.parent   = parent
+            self.layout   = QtGui.QVBoxLayout()
+            self.layoutR1 = QtGui.QHBoxLayout()
+            self.layoutR2 = QtGui.QHBoxLayout()
+            self.layoutR3 = QtGui.QHBoxLayout()
 
-    def __init__(self,name,imageItem=None,inputData=None,processedData=None):
-        self.name=name
-        if imageItem is not None:
-            self.imageItem=imageItem
-        else :
-            self.imageItem=pg.ImageItem(border='r')
-        self.inputData=inputData
-        self.processedData=processedData
+            self.nameLabel = QtGui.QLabel('\n'+parent.name)
+            self.layout.addWidget(self.nameLabel)
+            self.layout.addLayout(self.layoutR1)
+            self.layout.addLayout(self.layoutR2)
+            self.layout.addLayout(self.layoutR3)
 
-        self.ui = Layer.Ui()
+            self.setupLayoutR1()
+            self.setupLayoutR2()
+            self.setupLayoutR3()
+
+        def setupLayoutR1(self):
+            self.comboBoxImageType = QtGui.QComboBox()
+            self.comboBoxImageType.addItems(['Rgb','Lab','1C'])
+            self.channelSliderChannelDesc=QtGui.QLabel("C:")
+            self.sliderChannels = QtGui.QSlider(QtCore.Qt.Horizontal)
+            setPolicy(self.sliderChannels,QtGui.QSizePolicy.MinimumExpanding)
+            self.sliderChannels.setMinimum(0)
+            self.sliderChannels.setMaximum(10)
+            self.sliderChannels.setTickInterval(1)
+            self.sliderChannels.setSingleStep(1)
+            self.sliderChannels.setValue(0)
+            self.sliderChannels.setTickPosition(QtGui.QSlider.TicksAbove)
+            self.labelCurrentChannel=QtGui.QLabel(str(self.sliderChannels.sliderPosition()))
+
+            self.layoutR1.addWidget(self.comboBoxImageType)
+            self.layoutR1.addWidget(self.channelSliderChannelDesc)
+            self.layoutR1.addWidget(self.sliderChannels)
+            self.layoutR1.addWidget(self.labelCurrentChannel)
+
+        def setupLayoutR2(self):
+            self.colormap  = pg.GradientWidget()
+            
+            setPolicy(self.colormap)
+            self.colormap.setLength(5)
+            self.colormap.update()
+            self.layoutR2.addWidget(self.colormap)
+
+
+        def setupLayoutR3(self):
+            self.labelAlphaDesc=QtGui.QLabel("Alpha:")
+            self.sliderAlpha = QtGui.QSlider(QtCore.Qt.Horizontal)
+            setPolicy(self.sliderAlpha,QtGui.QSizePolicy.MinimumExpanding)
+            self.sliderAlpha.setGeometry(10, 10, 20, 30)
+            self.sliderAlpha.setMinimum(0)
+            self.sliderAlpha.setMaximum(100)
+            self.sliderAlpha.setTickInterval(1)
+            self.sliderAlpha.setSingleStep(1)
+            self.sliderAlpha.setValue(50)
+            self.labelAlpha=QtGui.QLabel(str(self.sliderAlpha.sliderPosition()/100.0))
+
+
+
+            self.layoutR3.addWidget(self.labelAlphaDesc)
+            self.layoutR3.addWidget(self.sliderAlpha)
+            self.layoutR3.addWidget(self.labelAlpha)
+
+            # connections
+            self.sliderAlpha.valueChanged.connect(self.parent.onAlphaChanged) 
+
+     
+        def initialzie(self):
+            self.parent.onAlphaChanged(25)
+
+    def onAlphaChanged(self,value):
+        self.ui.sliderAlpha.setValue(value)
+        self.alpha=float(value)/100.0
+        self.ui.labelAlpha.setText("%.2f" % self.alpha)
+
+        print "call processInputData (alpha change)"
+        self.processInputData()
+
+
+    def __init__(self,name,imageItem=None):
+
+        # name of the layer 
+        self.name           = name
+        # image item (item will be added to external view box)
+        self.imageItem=imageItem
+        if imageItem is None:
+            self.imageItem      = pg.ImageItem(border='r')
+
+        # attributes related to inout data
+        # (will be set from self.setImage )
+        self.inputData      = None
+        self.processedData  = None
+        self.nChannels      = None
+
+        # attributes which will be actualized from ui
+        self.alpha          = None
+        self.ui             = Layer.Ui(self)
+        self.ui.initialzie()
+        print "alpha after ui setup",self.alpha
+
+    def processInputData(self):
+        if self.inputData is None:
+            return
+
+        self.imageItem.setImage(self.inputData,opacity=self.alpha)
+
 
     def setImage(self,data):
         self.inputData=data
-        self.imageItem.setImage(data)
+        self.processInputData()
 
 
 class LayerdViewer(QtGui.QWidget):
@@ -34,6 +151,8 @@ class LayerdViewer(QtGui.QWidget):
         self.viewAndRightLayout= QtGui.QHBoxLayout()
         self.belowViewLayout= QtGui.QHBoxLayout()
         
+
+
         self.mainVLayout.addLayout(self.viewAndRightLayout)
         self.mainVLayout.addLayout(self.belowViewLayout)
         
@@ -50,10 +169,10 @@ class LayerdViewer(QtGui.QWidget):
     def setupRightToViewUi(self):
         self.rightToViewLayout=QtGui.QVBoxLayout()
         self.viewAndRightLayout.addLayout(self.rightToViewLayout)
-        toyBt1 = QtGui.QPushButton("testRight1")
-        toyBt2 = QtGui.QPushButton("testRight2")
-        self.rightToViewLayout.addWidget(toyBt1)
-        self.rightToViewLayout.addWidget(toyBt2)
+        #toyBt1 = QtGui.QPushButton("testRight1")
+        #toyBt2 = QtGui.QPushButton("testRight2")
+        #self.rightToViewLayout.addWidget(toyBt1)
+        #self.rightToViewLayout.addWidget(toyBt2)
         self.rightToViewLayout.addStretch(1)
 
     def setupViewBoxUi(self):
@@ -69,6 +188,12 @@ class LayerdViewer(QtGui.QWidget):
 
         
         self.viewAndRightLayout.addWidget(self.graphView)
+
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding,
+                                                 QtGui.QSizePolicy.MinimumExpanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        self.graphView.setSizePolicy(sizePolicy)
 
         # view box
         self.viewBox = pg.ViewBox()
@@ -97,36 +222,30 @@ class LayerdViewer(QtGui.QWidget):
 
     def addLayerGui(self,name):
         layer = self.layers[name]
-        layout = layer.ui.layout
-        layout = QtGui.QHBoxLayout()
-        self.rightToViewLayout.addLayout(layout)
-        
-        toyBt1 = QtGui.QPushButton(layer.name+'1')
-        toyBt2 = QtGui.QPushButton(layer.name+'2')
-        toyBt3 = QtGui.QPushButton(layer.name+'3')
-        layout.addWidget(toyBt1)
-        layout.addWidget(toyBt2)
-        layout.addWidget(toyBt3)
-        layer.ui.layout=layout
+        self.rightToViewLayout.addLayout(layer.ui.layout)
 
     def removeLayer(self,name):
         layer = self.layers[name]
-        self.viewBox.removeItem(layer.imageItem)
         self.removeLayerGui(name)
+        self.viewBox.removeItem(layer.imageItem)
         del self.layers[name]
 
     def removeLayerGui(self,name):
         layer = self.layers[name]
-        layoutToRemove = layer.ui.layout
+        self.delLayout(layer.ui.layoutR1,layer.ui.layout)
+        self.delLayout(layer.ui.layoutR2,layer.ui.layout)
+        self.delLayout(layer.ui.layoutR3,layer.ui.layout)
+        self.delLayout(layer.ui.layout,self.rightToViewLayout)
+
+    def delLayout(self,layoutToRemove,parent):
         for i in range(0, layoutToRemove.count()):
             layoutToRemove.itemAt(i).widget().hide()
-        self.rightToViewLayout.removeItem(layoutToRemove)
-
-   
+        parent.removeItem(layoutToRemove)
 
     def setLayerData(self,name,data):
+        flippedData=np.fliplr(data)
         layer = self.layers[name]
-        layer.setImage(data)
+        layer.setImage(flippedData)
 
 
     def show(self):
@@ -148,16 +267,16 @@ viewer.show()
 
 
 # add layers
-data1 = np.random.normal(size=(600, 300), loc=1024, scale=64).astype(np.float32)
+data1 = vigra.readImage('/home/tbeier/Desktop/lena.bmp')[:,:,:]
 viewer.addLayer('data1')
 viewer.setLayerData('data1',data1)
 
 
-data2 = np.random.normal(size=(600, 200), loc=1024, scale=64).astype(np.float32)
+data2 = vigra.readImage('/home/tbeier/Desktop/lena.bmp')[:,:,2]
 viewer.addLayer('data2')
 viewer.setLayerData('data2',data2)
 
-viewer.removeLayer('data2')
+#viewer.removeLayer('data2')
 
 
 QtGui.QApplication.instance().exec_()
